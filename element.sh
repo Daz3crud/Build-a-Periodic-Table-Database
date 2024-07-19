3db99a37b9b683a8eac9349926d1497165cd268f
Daz3crud SOFT <lukydaze@gmail.com>
2024-07-19 23:54:00 +0000

    fix: correct element.sh output and handle errors

--- element.sh
+++ element.sh
@@ -1,28 +1,39 @@
 #!/bin/bash
+PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
 
-PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+MAIN_PROGRAM() {
+  if [[ -z $1 ]]
+  then
+    echo "Please provide an element as an argument."
+  else
+    PRINT_ELEMENT $1
+  fi
+}
 
-if [[ -z $1 ]]; then
-  echo "Please provide an element as an argument."
-  exit 1
-fi
+PRINT_ELEMENT() {
+  INPUT=$1
+  if [[ ! $INPUT =~ ^[0-9]+$ ]]
+  then
+    ATOMIC_NUMBER=$(echo $($PSQL "SELECT atomic_number FROM elements WHERE symbol='$INPUT' OR name='$INPUT';") | sed 's/ //g')
+  else
+    ATOMIC_NUMBER=$(echo $($PSQL "SELECT atomic_number FROM elements WHERE atomic_number=$INPUT;") | sed 's/ //g')
+  fi
+  
+  if [[ -z $ATOMIC_NUMBER ]]
+  then
+    echo "I could not find that element in the database."
+  else
+    ELEMENT_INFO=$($PSQL "SELECT elements.atomic_number, elements.name, elements.symbol, types.type, properties.atomic_mass, properties.melting_point_celsius, properties.boiling_point_celsius FROM elements JOIN properties ON elements.atomic_number=properties.atomic_number JOIN types ON properties.type_id=types.type_id WHERE elements.atomic_number=$ATOMIC_NUMBER;")
+    
+    echo "$ELEMENT_INFO" | while IFS=" | " read ATOMIC_NUMBER NAME SYMBOL TYPE ATOMIC_MASS MELTING_POINT_CELSIUS BOILING_POINT_CELSIUS
+    do
+      echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT_CELSIUS celsius and a boiling point of $BOILING_POINT_CELSIUS celsius."
+    done
+  fi
+}
 
-# Check if input is a number (atomic_number)
-if [[ $1 =~ ^[0-9]+$ ]]; then
-  ELEMENT_RESULT=$($PSQL "SELECT atomic_number, name, symbol FROM elements WHERE atomic_number=$1")
-else
-  ELEMENT_RESULT=$($PSQL "SELECT atomic_number, name, symbol FROM elements WHERE symbol='$1' OR name='$1'")
-fi
+START_PROGRAM() {
+  MAIN_PROGRAM $1
+}
 
-if [[ -z $ELEMENT_RESULT ]]; then
-  echo "I could not find that element in the database."
-  exit 1
-fi
-
-echo "$ELEMENT_RESULT" | while IFS="|" read ATOMIC_NUMBER NAME SYMBOL; do
-  PROPERTIES_RESULT=$($PSQL "SELECT atomic_mass, melting_point_celsius, boiling_point_celsius, type FROM properties JOIN types ON properties.type_id = types.type_id WHERE atomic_number=$ATOMIC_NUMBER")
-
-  echo "$PROPERTIES_RESULT" | while IFS="|" read ATOMIC_MASS MELTING_POINT BOILING_POINT TYPE; do
-    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
-  done
-done
+START_PROGRAM $1
